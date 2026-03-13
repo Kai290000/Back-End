032fade3b045d709342bc68ece9288de8a516ac4
Kai <vitor.batista.bueno@escola.pr.gov.br>
2026-03-13 19:17:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const next = document.querySelectorAll('.btn-proximo');
 
+next.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativa')
+        const proximaExplicacao = 'explicacao-' + this.getAttribute('data-proximo')
+
+        atual.classList.remove('ativa')
+        document.getElementById(proximaExplicacao).classList.add('ativa')
+    })
+})
